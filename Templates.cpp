d145e1c541ecca3f7a54779162664f80d39ab52c
rxm3n <41157757+RayyuShao@users.noreply.github.com>
2023-07-28 21:39:13 -0400

    Created main function Includes an int, a float, and a double, which can be adjusted as wanted The halves of each number of each type is outputted

--- Templates.cpp
+++ Templates.cpp
@@ -18,4 +18,14 @@ typename enable_if<is_integral<T>::value, T>::type half(T value) {
     return (value + (value % 2)) / 2;
 }
 
+int main() {
+    int intValue = 7;   // integer number; adjust as wanted
+    float floatValue = 5.0f;    // floating-point number; adjust as wanted
+    double doubleValue = 3.0;     // double type number; adjust as wanted
 
+    cout << "Half of the int " << intValue << " is: " << half(intValue) << endl;    // cout half of int
+    cout << "Half of the float " << floatValue << " is: " << half(floatValue) << endl;  // cout half of float
+    cout << "Half of the double " << doubleValue << " is: " << half(doubleValue) << endl;   // cout half of double
+
+    return 0;
+}
