fa64addc258801df97051a796bc659e5a1554296
rxm3n <41157757+RayyuShao@users.noreply.github.com>
2023-07-28 21:35:34 -0400

    Created template function for int numbers, which accepts one value and returns half of the value according to regular rounding rules

--- Templates.cpp
+++ Templates.cpp
@@ -4,27 +4,18 @@
 
 #include <iostream>
 #include <type_traits>
+using namespace std;
 
 // Template function for floating-point types
 template <typename T>
-typename std::enable_if<std::is_floating_point<T>::value, T>::type half(T value) {
+typename enable_if<is_floating_point<T>::value, T>::type half(T value) {
     return value / 2;
 }
 
 // Template function for integer types
 template <typename T>
-typename std::enable_if<std::is_integral<T>::value, T>::type half(T value) {
+typename enable_if<is_integral<T>::value, T>::type half(T value) {
     return (value + (value % 2)) / 2;
 }
 
-int main() {
-    int intValue = 7;
-    float floatValue = 10.5;
-    double doubleValue = 12.7;
 
-    std::cout << "Half of " << intValue << " is: " << half(intValue) << std::endl;
-    std::cout << "Half of " << floatValue << " is: " << half(floatValue) << std::endl;
-    std::cout << "Half of " << doubleValue << " is: " << half(doubleValue) << std::endl;
-
-    return 0;
-}
